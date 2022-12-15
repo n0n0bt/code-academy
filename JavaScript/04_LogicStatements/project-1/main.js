3bb79d04347588708a8782d4602cc5c3a0b9956a
n0n0bt <naumejosevski@gmail.com>
2022-12-15 18:03:30 +0100

    Mod

--- JavaScript/04_LogicStatements/project-1/main.js
+++ JavaScript/04_LogicStatements/project-1/main.js
@@ -16,65 +16,23 @@
 
 
 // Smaller Version :
-// let randomNum = Math.floor(Math.random() * 11);
-// console.log("The Computer choose: " + randomNum);
+let randomNum = Math.floor(Math.random() * 11);
+console.log("The Computer choose: " + randomNum);
 
-// let userInput = parseInt(prompt("Enter a number between 0 - 10"));
-// console.log("User Input: " + userInput + " type of: " + typeof userInput);
+let userInput = parseInt(prompt("Enter a number between 0 - 10"));
+console.log("User Input: " + userInput + " type of: " + typeof userInput);
 
-// if (userInput > randomNum) {
-//     console.log("Your number is greater than the random number");
-// } else if(userInput < randomNum) {
-//     console.log("Your number is smaller than the random number");
-// } else {
-//     console.log("You guessed the number!");
-// }
+if (userInput > randomNum) {
+    console.log("Your number is greater than the random number");
+} else if(userInput < randomNum) {
+    console.log("Your number is smaller than the random number");
+} else {
+    console.log("You guessed the number!");
+}
 
 
 // the smallest version :
 
-// let message = userInput > randomNum ? "Your number is greater than the random number" : userInput < randomNum ?
-// "Your number is smaller than the random number" : "You guessed the number!";
-// console.log(message);
-
-
-
-let max = parseInt(prompt("Enter max value for guessing."));
-if (isNaN(max)) {
-    let isNumber = false;
-    while (!isNumber) {
-        max = parseInt(prompt("Enter max value for guessing."));
-        if (!isNaN(max)) {
-            isNumber = true; 
-        }
-    }
-}  
-
-max++;
-let guessingNumber = Math.floor(Math.random() * max);
-alert(guessingNumber);
-
-let rightNumber = false;
-
-max--;
-while (!rightNumber) {
-    let userNumber = parseInt(prompt("Enter a number between o and " + max));
-    if (isNaN(userNumber)) {
-        let isNumber = false;
-        while (!isNumber) {
-            userNumber = parseInt(prompt("Enter a number between o and " + max));
-            if (!isNan(userNumber)) {
-                isNumber = true; 
-            }
-        }
-    }  
-    
-    if (guessingNumber === userNumber) {
-        alert("You guessed right! You won the game!");
-        rightNumber = true;
-    } else if (guessingNumber < userNumber) {
-        alert("You guessed highter then the number!");
-    } else {
-        alert("You guessed lower then the number!");
-    }
-}
+let message = userInput > randomNum ? "Your number is greater than the random number" : userInput < randomNum ?
+"Your number is smaller than the random number" : "You guessed the number!";
+console.log(message);
