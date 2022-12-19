beabdac43275a654130e82ce83c944afcd527b5e
n0n0bt <naumejosevski@gmail.com>
2022-12-19 11:48:44 +0100

    some fixes

--- JavaScript/exercise-saturday/ex-17.12/while-loop/main.js
+++ JavaScript/exercise-saturday/ex-17.12/while-loop/main.js
@@ -1,4 +1,5 @@
 
+// WRITE JS CODE TO PRINT NUMBERS FROM 1 TO 10 IN A SINGLE LINE //
 
 // let i = 0;
 // let arr = [];
@@ -7,15 +8,25 @@
 //     arr.push(i);
 //     i++;
 // }
+// console.log(`The numbers are: ${arr}`);
 
-// console.log(arr);
+
+///////// THE SECOND WAY TO DO -
+
+
+// let arr = [];
+// for (let index = 0; index <= 10; index++) {
+//     arr.push(index);
+// }
+
+// console.log(`The number are: ${arr}`);
+
 
 
 /////////////////////////////////////////////////////////////////
+// WRITE A JS CODE TO FIND THE NUMBER OF DIGITS IN A NUMBER //
 
-
-
-// let num = 12455654;
+// let num = 124556541;
 // let count = 0;
 
 // while (num != 0) {
@@ -23,11 +34,12 @@
 //     count++;
 // }
 
-// let arr1 = 12455654;
-// console.log(`The number of digits of the numberr: ${arr1} is ${count}`);
+// let arr1 = 124556541;
+// console.log(`The number of digits of the number: ${arr1} is ${count}`);
 
 
 /////////////////////////////////////////////////////////////////
+// PRINT THE ODD NUMBER FROM 1 TO 100 USE ARRAY TO STORE THE NUMBERS //
 
 
 // let arr = [];
@@ -42,7 +54,20 @@
 // console.log(arr);
 
 
+////////////////// SECOND WAY TO DO -
+
+// let arr = [];
+// for (let i = 0; i <= 100; i++) {
+//     if(i % 2 != 0){
+//         arr.push(i);
+//         i++;
+//     }
+    
+// }
+// console.log(arr);
+
 /////////////////////////////////////////////////////////////////
+// WRITE JS CODE TO PRINT NUMBERS FRORM GIVEN RANGE BY INPUT
 
 
 // let userInput1 = parseInt(prompt("Write the first number:"));
@@ -55,13 +80,15 @@
 
 
 /////////////////////////////////////////////////////////////////
+// GUESS A GENERATED SECRET NUMBER BETWEEN 1 AND 10 //
 
 
 // let secretNumber = Math.floor(Math.random() * 10);
 // let guesses = 0; // for storing the number of guesses
+
 // do {
 //   // get input from user
-//   let number = parseInt(prompt("Please enter a number between 0 and 10"));
+//   number = parseInt(prompt("Please enter a number between 0 and 10"));
 //   // increase the number of guesses
 //   guesses++;
 //   // check input number with the secret number provide hint if needed
@@ -77,31 +104,32 @@
 // } while (number != secretNumber);
 
 
-
+// FOR LOOP
 /////////////////////////////////////////////////////////////////
+// MAKE A LOOP OF TWO NUMBERS AND PRINT THE SUM OF THEM
 
 // for (let i = 0; i < 3; i++) {
 //     for (let j = 0; j < 3; j++) {
-//         console.log(`The sum of ${i} and ${j} is: ${i + j}`);
+//       console.log(`The sum of ${i} and ${j} is: ${i + j}`);
 //     }
     
 // }
 
 /////////////////////////////////////////////////////////////////
+// WRITE A JS CODE TO PRINT EVEN NUMBERS IN GIVEN ARRAY
 
 
 // let arr = [1,2,3,4,5,6,7,8,9];
 
-// let message = "";
-
 // for (let i = 0; i < arr.length; i++) {
 //     if (arr[i] % 2 != 0) {
-//         console.log(arr[i]);
+//       console.log(arr[i]);
 //     }
 // }
 
 
 /////////////////////////////////////////////////////////////////
+// MAKE A PRODUCT OF TWO ARRAYS OF THE SAME SIZE USING FOR LOOP //
 
 // let arr1 = [2,4,6,7,8];
 // let arr2 = [4,6,1,5,0];
@@ -113,8 +141,17 @@
     
 // }
 
+// A MAP() VERSION -
+
+// let arr1 = [2,4,6,7,8];
+// let arr2 = [4,6,1,5,0];
+// let product = [];
+// let productArr = arr1.map((x, i) => x * arr2[i]);
+// console.log(productArr);
+
 
 ////////////////////////////////////////////////////////////////
+// WRITE A JS CODE TO FIND THE LARGEST NUMBER IN A ARRAY //
 
 
 
@@ -132,7 +169,7 @@
 
 
 /////////////////////////////////////////////////////////////////
-
+// WRITE A JS CODE TO FIND THE SMALLEST NUMBER IN A ARRAY //
 
 // let arr = [1,2,60,40,2,0];
 
@@ -158,6 +195,8 @@
 //         console.log("FIZZ");
 //     }else if (i % 5 == 0) {
 //         console.log("BUZZ");
+//     }else{
+//       console.log(i);
 //     }
 // }
 
@@ -166,12 +205,13 @@
 
 // let cars = ["BMW","AUDI","SKODA","YUGO","RENAULT","FI4O",];
 
-// for (const obj of cars) {
+// for (let obj of cars) {
 //     console.log(obj);
 // }
 
 
 /////////////////////////////////////////////////////////////////
+// List the property from the following object //
 
 // let restourant = {
 //     name: "REMO",
