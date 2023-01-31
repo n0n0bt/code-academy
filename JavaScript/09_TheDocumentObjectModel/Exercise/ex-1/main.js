b480c9b44bee2098b48230c3a49a53679241663a
n0n0bt <naumejosevski@gmail.com>
2023-01-31 20:58:09 +0100

    31.01.2023

---
+++ JavaScript/09_TheDocumentObjectModel/Exercise/ex-1/main.js
@@ -0,0 +1,9 @@
+let h1 = document.querySelector("h1");
+console.dir(h1);
+
+h1.innerText = "What's Poppin?";
+
+let elementArr = document.querySelectorAll(".output");
+console.dir(elementArr);
+
+elementArr[2].innerText = "Hello World good afternoon";
